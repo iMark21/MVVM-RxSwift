95b6f27a10e719ee39680e3becd10a0b27386390
Juan Miguel Marques Morilla <juan.marques@smallworldfs.com>
2018-11-16 10:57:33 +0100

    Initial Commit

---
+++ CryptoCompare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoCompare
+//
+//  Created by Juan Miguel Marques Morilla on 16/11/2018.
+//  Copyright © 2018 Juan Miguel Marques Morilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CryptoCompareTests/CryptoCompareTests.swift
@@ -0,0 +1,34 @@
+//
+//  CryptoCompareTests.swift
+//  CryptoCompareTests
+//
+//  Created by Juan Miguel Marques Morilla on 16/11/2018.
+//  Copyright © 2018 Juan Miguel Marques Morilla. All rights reserved.
+//
+
+import XCTest
+@testable import CryptoCompare
+
+class CryptoCompareTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
